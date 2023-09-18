925b9f47a1127b2b71f158bf438016cae6b481e0
Franciszek Sośniak <projeapp2@gmail.com>
2023-09-18 19:44:39 +0200

    Kalkulator - Dec -> 1-10

---
+++ Kalkulator/main.py
@@ -0,0 +1,27 @@
+import logging
+
+
+base = int(input('Podaj podstawę sytemu: '))
+sel =  input('Wybierz operację: \n1. Z systemu obcego na sytem dziesiętny \n2. Z systemu dziesiętnego na system obcy \n')
+
+def toAlien(input):
+    cur = input
+    result = ''
+    while cur > 0:
+        result += str(cur % base)
+        cur = cur // base
+    print(result[::-1])
+
+def toDec(input):
+    #TODO: Zrobić to
+    print('test')
+
+if sel == 1:
+    alien = str(input('Podaj liczbę w sytemie obcym: '))
+else:
+    dec = int(input('Podaj liczbę w sytemie dziesiętnym: '))
+    toAlien(dec)
+
+
+
+
