fcffd1194f1eff3a979ef6cfbbe1da9f1a0a68ac
FraI3mega <projeapp2@gmail.com>
2023-09-19 18:00:58 +0200

    konwerter - toAlien

--- Konwerter/konwerter.py
+++ Konwerter/konwerter.py
@@ -1,16 +1,48 @@
-import logging
-
-
 base = int(input('Podaj podstawę sytemu: '))
 sel =  input('Wybierz operację: \n1. Z systemu obcego na sytem dziesiętny \n2. Z systemu dziesiętnego na system obcy \n')
 
+toDecL = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15
+}
+
+toAlienL = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 'A',
+    '11': 'B',
+    '12': 'C',
+    '13': 'D',
+    '14': 'E',
+    '15': 'F'
+}
 def toAlien(input):
     cur = input
     result = ''
     if base != 1:
         # Gdy podstawa systemu nie jest cyfrą jeden
         while cur > 0:
-            result += str(cur % base)
+            result += toAlienL[str(cur % base)]
             cur = cur // base
     else:
         # Gdy podstawa jest cyfrą jeden dodaj do wyniki tyle jedynek jaką ma wartość wejściowa
@@ -20,15 +52,14 @@ def toAlien(input):
     print(result[::-1])
 
 def toDec(input):
-    #TODO: Zrobić to
-    print('TODO')
+    result = 0
+    for x in input:
+        result = result * base + toDecL[x]
+    print(result)
 
-if sel == 1:
+if sel == '1':
     alien = str(input('Podaj liczbę w sytemie obcym: '))
+    toDec(alien)
 else:
     dec = int(input('Podaj liczbę w sytemie dziesiętnym: '))
     toAlien(dec)
-
-
-
-
