cc67b95f5ef90c65e45dab15d3bf9f2546744bba
FraI3mega <projeapp2@gmail.com>
2023-09-19 17:20:19 +0200

    konwerter - naprawa błędu przy sytemie jedynkowym

--- Konwerter/konwerter.py
+++ Konwerter/konwerter.py
@@ -5,17 +5,23 @@ base = int(input('Podaj podstawę sytemu: '))
 sel =  input('Wybierz operację: \n1. Z systemu obcego na sytem dziesiętny \n2. Z systemu dziesiętnego na system obcy \n')
 
 def toAlien(input):
-    #FIXME: system jedynkowy
     cur = input
     result = ''
-    while cur > 0:
-        result += str(cur % base)
-        cur = cur // base
+    if base != 1:
+        # Gdy podstawa systemu nie jest cyfrą jeden
+        while cur > 0:
+            result += str(cur % base)
+            cur = cur // base
+    else:
+        # Gdy podstawa jest cyfrą jeden dodaj do wyniki tyle jedynek jaką ma wartość wejściowa
+        while cur > 0:
+            result += '1'
+            cur -= 1
     print(result[::-1])
 
 def toDec(input):
     #TODO: Zrobić to
-    print('test')
+    print('TODO')
 
 if sel == 1:
     alien = str(input('Podaj liczbę w sytemie obcym: '))
