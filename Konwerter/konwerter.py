bd79c9fa5f72c4c7f9bd2fdd0aa94e67bd4e2f2f
FraI3mega <projeapp2@gmail.com>
2023-10-08 13:47:05 +0200

    Program działa jako pętla teraz. Naprawa błędu z zamianą. Restrukturyzacja.

--- Konwerter/konwerter.py
+++ Konwerter/konwerter.py
@@ -1,7 +1,10 @@
-base = int(input('Podaj podstawę sytemu: '))
-sel =  input('Wybierz operację: \n1. Z systemu obcego na sytem dziesiętny \n2. Z systemu dziesiętnego na system obcy \n')
+''' Konwerter systemów liczbowych
+    DEC <-> do HEX
+    FraI3mega @ 2023
+'''
 
 toDecL = {
+    '0': 0,
     '1': 1,
     '2': 2,
     '3': 3,
@@ -20,46 +23,66 @@ toDecL = {
 }
 
 toAlienL = {
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    '10': 'A',
-    '11': 'B',
-    '12': 'C',
-    '13': 'D',
-    '14': 'E',
-    '15': 'F'
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'A',
+    11: 'B',
+    12: 'C',
+    13: 'D',
+    14: 'E',
+    15: 'F'
 }
+
+base = 0
+
 def toAlien(input):
     cur = input
     result = ''
     if base != 1:
         # Gdy podstawa systemu nie jest cyfrą jeden
         while cur > 0:
-            result += toAlienL[str(cur % base)]
+            result += toAlienL[cur % base]
             cur = cur // base
     else:
         # Gdy podstawa jest cyfrą jeden dodaj do wyniki tyle jedynek jaką ma wartość wejściowa
         while cur > 0:
             result += '1'
             cur -= 1
-    print(result[::-1])
+    print('Wynik: ' + result[::-1] + "\n")
 
 def toDec(input):
     result = 0
     for x in input:
         result = result * base + toDecL[x]
-    print(result)
+    print('Wynik: ' + str(result) + "\n")
 
-if sel == '1':
-    alien = str(input('Podaj liczbę w sytemie obcym: '))
-    toDec(alien)
-else:
-    dec = int(input('Podaj liczbę w sytemie dziesiętnym: '))
-    toAlien(dec)
+while True:
+    if base == 0:
+        base = int(input('Podaj podstawę sytemu: '))
+    
+    sel =  int(input('Wybierz operację: \n1. Z systemu obcego na sytem dziesiętny \n2. Z systemu dziesiętnego na system obcy \n3. Zmień podstawę systemu\n4. Wyjdź z programu\n> '))
+
+    if sel == 1:
+        alien = str(input('Podaj liczbę w sytemie obcym: '))
+        toDec(alien)
+
+    elif sel == 2:
+        dec = int(input('Podaj liczbę w sytemie dziesiętnym: '))
+        toAlien(dec)
+    
+    elif sel == 3:
+        base = int(input('Podaj podstawę sytemu: '))
+    
+    elif sel == 4:
+        break
+
+    else:
+        print('Błedny wybór\n')
